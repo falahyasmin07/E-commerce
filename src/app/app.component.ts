29e90cd27472a2c2e2db422851432f5fe9ebb4a4
Riadh-design <58599160+Riadh-design@users.noreply.github.com>
2025-03-24 23:59:28 +0100

    intermediate commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeroComponent } from './components/hero/hero.component';
 import { BuildDeploySecureComponent } from './components/build-deploy-secure/build-deploy-secure.component';
 import { SolutionsComponent } from './components/solutions/solutions.component';
@@ -6,14 +7,14 @@ import { CaseStudiesComponent } from './components/case-studies/case-studies.com
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ResourcesComponent } from './components/resources/resources.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, BuildDeploySecureComponent, SolutionsComponent, CaseStudiesComponent, AboutUsComponent, ResourcesComponent, ContactUsComponent, CommonModule],
+  imports: [CommonModule, HeroComponent, BuildDeploySecureComponent, SolutionsComponent, CaseStudiesComponent, AboutUsComponent, ResourcesComponent, ContactUsComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'edge-pulsar-website';
