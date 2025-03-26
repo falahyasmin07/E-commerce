c737c9c0d1998f0a5937f1d269766647e20daccc
Riadh-design <58599160+Riadh-design@users.noreply.github.com>
2025-03-26 12:14:01 +0100

    avoid duplication and routing of home to offers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { BuildPageComponent } from './components/build-page/build-page.component';
 import { RunPageComponent } from './components/run-page/run-page.component';
 import { SecurePageComponent } from './components/secure-page/secure-page.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'build', component: BuildPageComponent },
   { path: 'run', component: RunPageComponent },
   { path: 'secure', component: SecurePageComponent }
