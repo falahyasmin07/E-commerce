2491d758e2de68b14e02d803278a3ba3ec442f27
Riadh-design <58599160+Riadh-design@users.noreply.github.com>
2025-03-27 08:50:35 +0100

    cleaning RUN page

--- src/app/components/run-page/run-page.component.ts
+++ src/app/components/run-page/run-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-run-page',
   templateUrl: './run-page.component.html',
-  styleUrls: ['./run-page.component.css']
+  styleUrls: ['./run-page.component.css'],
+  imports: [HeroComponent]
 })
 export class RunPageComponent {
 
--- src/app/components/secure-page/secure-page.component.ts
+++ src/app/components/secure-page/secure-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-secure-page',
   templateUrl: './secure-page.component.html',
-  styleUrls: ['./secure-page.component.css']
+  styleUrls: ['./secure-page.component.css'],
+  imports: [HeroComponent]
 })
 export class SecurePageComponent {
 
