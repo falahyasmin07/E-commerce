09bd1898d73f8a0a675510377f57a2279f251627
Mazen Neifer <mazen.neifer@edgepulsar.com>
2025-04-11 11:45:39 +0200

    WEB/#2: Reworked About Us section in home web page.
    Added founders biography and images.
    
    Closes #2.

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-about-us',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './about-us.component.html',
-  
+	selector: 'app-about-us',
+	imports: [CommonModule],
+	templateUrl: './about-us.component.html',
+	styleUrl: './about-us.component.css'
 })
-export class AboutUsComponent {
 
+export class AboutUsComponent {
+	teamMembers = [
+		{
+			name: 'Mazen Neifer',
+			image: 'assets/images/Mazen NEIFER.png',
+			bio: 'Mazen Neifer is the Chairman and CTO of Edge Pulsar. He worked as Cloud Development Director at Nestwave between September 2015 and July 2024. He started his career in 2001 at Atmel. Later he joined Sequans as a 4G project manager and technical leader since August 2005. In 2013, he worked at NXP Semiconductors as DSP firmware architect. Mazen graduated in 2001 from ISAE-SUPAERO in electronics and telecommunications. He contributed to many Open Source projects like FreeRTOS, Zephir and Debian.'
+		},
+		{
+			name: 'Riadh Berhouma',
+			image: 'assets/images/Riadh BERHOUMA.jpg',
+			bio: 'Riadh Berhoma is the CEO at Edge Pulsar. With a strong background in technology and business management, he has significantly contributed to innovation and growth in companies like Valeo, Sequans and Texas Instruments. His expertise in strategic planning and project management has been vital for their success. Riadh holds an MBA as well as a degree in engineering and has extensive experience in various technical and managerial roles, making him a key figure in the technology sector.'
+		},
+	];
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SolutionsComponent } from 'src/app/components/solutions/solutions.component';
+import { AboutUsComponent } from 'src/app/components/about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SolutionsComponent],
+  imports: [AboutUsComponent, SolutionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
