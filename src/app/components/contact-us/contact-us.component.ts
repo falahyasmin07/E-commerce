64e537cf82c194dc4d231820bc204e480c09e4d4
Mazen Neifer <mazen.neifer@edgepulsar.com>
2025-04-12 10:36:17 +0200

    WEB/#9: Updated Contact Us section.
    Updated phone and mail address.
    
    Added Google Maps location.
    
    Added form to send data (no BE support yet).

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+declare var hbspt: any;
 
 @Component({
-  selector: 'app-contact-us',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './contact-us.component.html',
-  
+	selector: 'app-contact-us',
+	imports: [CommonModule, FormsModule],
+	templateUrl: './contact-us.component.html',
+	styleUrl: './contact-us.component.css'
 })
-export class ContactUsComponent {
 
+export class ContactUsComponent implements OnInit {
+	ngOnInit() {
+	}
+
+	onSubmit() {
+		// TODO: Add here a POST request to submit collected data
+		console.log('Form was submitted');
+	}
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SolutionsComponent } from 'src/app/components/solutions/solutions.component';
 import { AboutUsComponent } from 'src/app/components/about-us/about-us.component';
+import { ContactUsComponent } from 'src/app/components/contact-us/contact-us.component';
 
 @Component({
   selector: 'app-home',
-  imports: [AboutUsComponent, SolutionsComponent],
+  imports: [AboutUsComponent, ContactUsComponent, SolutionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
