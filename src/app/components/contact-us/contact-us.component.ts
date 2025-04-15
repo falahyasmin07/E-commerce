ac758c3f554591776c75977960d50465e513ec56
Mazen Neifer <mazen.neifer@edgepulsar.com>
2025-04-15 11:49:36 +0200

    WEB/#9: Enabled sending user data to backend server to be store in DB.
    The form data are collected and sent using a POST API call.
    
    The submit button is active only if data is filled in.
    
    There is no check on data validity. This can be added later.

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,22 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 declare var hbspt: any;
 
 @Component({
 	selector: 'app-contact-us',
-	imports: [CommonModule, FormsModule],
+	imports: [FormsModule, HttpClientModule],
 	templateUrl: './contact-us.component.html',
 	styleUrl: './contact-us.component.css'
 })
 
 export class ContactUsComponent implements OnInit {
+	constructor(private http: HttpClient) {
+	}
+
 	ngOnInit() {
 	}
 
-	onSubmit() {
-		// TODO: Add here a POST request to submit collected data
-		console.log('Form was submitted');
+	onSubmit(form: NgForm) {
+		// Build the URL dynamically based on the current location
+		const baseUrl = "https://edgepulsar.com"; //window.location.origin;
+		const url = `${baseUrl}/actions/db_store.php`;
+		console.log('Form to be submitted to BE server');
+		// Send the form data to the PHP script
+		console.log("data", form.value);
+		this.http.post(url, form.value).subscribe(
+			response => {
+				console.log('Success!', response);
+			},
+			error => {
+				console.error('Error!', error);
+			}
+		);
 	}
 }
