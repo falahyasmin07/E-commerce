4afe6b5e32d4c51c3a0ebf02f639af27bdaf6695
Mazen Neifer <mazen.neifer@edgepulsar.com>
2025-04-15 13:37:48 +0200

    Cleaned code removing compiler warnings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroComponent } from './components/hero/hero.component';
-import { BuildDeploySecureComponent } from './components/build-deploy-secure/build-deploy-secure.component';
-import { CaseStudiesComponent } from './components/case-studies/case-studies.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { ResourcesComponent } from './components/resources/resources.component';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeroComponent, BuildDeploySecureComponent, CaseStudiesComponent, AboutUsComponent, ResourcesComponent, ContactUsComponent, CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
