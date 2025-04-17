9f991d8cd8aeb0f29585799250e43c020a54e0f8
Mazen Neifer <mazen.neifer@edgepulsar.com>
2025-04-17 11:28:18 +0200

    WEB/#1: Created a new flip-card component to reduce copy/paste in code.
    This is part of the home page refactoring. It provides a way to make all
    cards similar in order to avoid accidental breakage of some of them.
    
    It helps reducing maintenance effort while increasing coherence of the
    visual rendering.

---
+++ src/app/components/flip-card/flip-card.component.ts
@@ -0,0 +1,68 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+@Component({
+	selector: 'app-flip-card',
+	templateUrl: './flip-card.component.html',
+	styleUrls: ['./flip-card.component.css'],
+	imports: [CommonModule],
+	animations: [
+		trigger('cardFlip', [
+			state('front', style({
+				transform: 'rotateY(0deg)',
+			})),
+			state('back', style({
+				transform: 'rotateY(180deg)',
+			})),
+			transition('front => back', animate('900ms ease-out')),
+			transition('back => front', animate('900ms ease-in'))
+		])
+	]
+})
+export class FlipCardComponent implements OnInit, OnDestroy {
+	@Input() images: string[] = [];
+	@Input() interval: number = 3000;
+	@Input() icon: string = '';
+	@Input() title: string = '';
+	@Input() alt: string = '';
+	@Input() path: string = '';
+
+	currentImageIndex = 0;
+	flipState: 'front' | 'back' = 'front';
+	private intervalId: any;
+	isFlipped: boolean = false;
+
+	ngOnInit() {
+		this.startCarousel();
+	}
+
+	startCarousel() {
+		if (this.images.length > 1) {
+			this.intervalId = setInterval(() => {
+				this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+			}, this.interval);
+		}
+	}
+
+	onHover() {
+		this.flipState = 'back';
+		setTimeout(() => { this.isFlipped = true }, 450);
+		clearInterval(this.intervalId);
+	}
+
+	onLeave() {
+		this.flipState = 'front';
+		setTimeout(() => { this.isFlipped = false }, 450);
+		this.startCarousel();
+	}
+
+	ngOnDestroy() {
+		clearInterval(this.intervalId);
+	}
+
+	imagePath(index: number) {
+		return this.path.replace('{image}', this.images[index]);
+	}
+}
+
