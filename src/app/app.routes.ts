bca40ad256558132d0dd318b4d41ee694106c1dc
Mazen Neifer <mazen.neifer@edgepulsar.com>
2025-05-13 12:10:43 +0200

    WEB/#26: Added page for presenting "Electronics" solutions.
    Took care in new page display on small media like phones.
    
    Added sub menu for solutions menu and connected new page.
    
    Close #26.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { BuildPageComponent } from './components/build-page/build-page.component';
 import { RunPageComponent } from './components/run-page/run-page.component';
 import { SecurePageComponent } from './components/secure-page/secure-page.component';
+import { ElectronicsPage } from './pages/electronics/electronics.page';
 import { IndustryPage } from './pages/industry/industry.page';
 import { HomePage } from './pages/home/home.page';
 
 export const routes: Routes = [
   { path: '', component: HomePage, pathMatch: 'full' },
   { path: 'build', component: BuildPageComponent },
+  { path: 'electronics', component: ElectronicsPage },
   { path: 'industry-4.0', component: IndustryPage },
   { path: 'run', component: RunPageComponent },
   { path: 'secure', component: SecurePageComponent }
---
+++ src/app/pages/electronics/electronics.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeroComponent } from 'src/app/components/hero/hero.component';
+
+@Component({
+	selector: 'app-page-electronics',
+	imports: [HeroComponent],
+	templateUrl: './electronics.page.html',
+	styleUrls: ['./electronics.page.css'],
+})
+export class ElectronicsPage {
+
+}
