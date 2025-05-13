f89cd27cf792ffc82e683f69af12472c1511da7e
Mazen Neifer <mazen.neifer@edgepulsar.com>
2025-05-13 15:58:56 +0200

    WEB/#24: Added page for presenting "Automotive" solutions.
    Took care in new page display on small media like phones.
    
    Added sub menu for solutions menu and connected new page.
    
    Closes #24.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { BuildPageComponent } from './components/build-page/build-page.component';
 import { RunPageComponent } from './components/run-page/run-page.component';
 import { SecurePageComponent } from './components/secure-page/secure-page.component';
+import { AutomotivePage } from './pages/automotive/automotive.page';
 import { ElectronicsPage } from './pages/electronics/electronics.page';
 import { IndustryPage } from './pages/industry/industry.page';
 import { LogisticsPage } from './pages/logistics/logistics.page';
@@ -9,7 +10,7 @@ import { HomePage } from './pages/home/home.page';
 
 export const routes: Routes = [
   { path: '', component: HomePage, pathMatch: 'full' },
-  { path: 'build', component: BuildPageComponent },
+  { path: 'automotive', component: AutomotivePage },
   { path: 'electronics', component: ElectronicsPage },
   { path: 'industry-4.0', component: IndustryPage },
   { path: 'logistics', component: LogisticsPage },
