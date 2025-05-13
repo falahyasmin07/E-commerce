1dbe0d16c98d666ad3a7d5d104487848c2a341fc
Mazen Neifer <mazen.neifer@edgepulsar.com>
2025-05-13 17:10:32 +0200

    WEB/#27: Added page for presenting "Energy" solutions.
    Took care in new page display on small media like phones.
    
    Added sub menu for solutions menu and connected new page.
    
    Closes #27.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RunPageComponent } from './components/run-page/run-page.component';
 import { SecurePageComponent } from './components/secure-page/secure-page.component';
 import { AutomotivePage } from './pages/automotive/automotive.page';
 import { ElectronicsPage } from './pages/electronics/electronics.page';
+import { EnergyPage } from './pages/energy/energy.page';
 import { IndustryPage } from './pages/industry/industry.page';
 import { LogisticsPage } from './pages/logistics/logistics.page';
 import { HomePage } from './pages/home/home.page';
@@ -12,6 +13,7 @@ export const routes: Routes = [
   { path: '', component: HomePage, pathMatch: 'full' },
   { path: 'automotive', component: AutomotivePage },
   { path: 'electronics', component: ElectronicsPage },
+  { path: 'energy', component: EnergyPage },
   { path: 'industry-4.0', component: IndustryPage },
   { path: 'logistics', component: LogisticsPage },
   { path: 'run', component: RunPageComponent },
---
+++ src/app/pages/energy/energy.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeroComponent } from 'src/app/components/hero/hero.component';
+
+@Component({
+	selector: 'app-page-electronics',
+	imports: [HeroComponent],
+	templateUrl: './energy.page.html',
+	styleUrls: ['./energy.page.css'],
+})
+export class EnergyPage {
+
+}
